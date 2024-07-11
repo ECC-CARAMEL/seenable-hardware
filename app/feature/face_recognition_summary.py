877bfa8a774cb713d11c81f8a5344ea4869850aa
AluFu <0hp3261131m102u@ezweb.ne.jp>
2024-07-11 11:29:11 +0900

    [update]unknownの時にサーバーにリクエストし画像の名前を取得するようにした

--- app/feature/face_recognition_summary.py
+++ app/feature/face_recognition_summary.py
@@ -1,29 +1,36 @@
 import cv2
 import face_recognition
 import os
+from PIL import Image, ImageDraw, ImageFont
+import numpy as np
+from .server_communication import send_to_server
 
 def load_training_images(train_img_directory, train_img_names):
     train_imgs = []
     for name in train_img_names:
         img_path = os.path.join(train_img_directory, name)
         if os.path.exists(img_path):
-            train_imgs.append(face_recognition.load_image_file(img_path))
+            train_imgs.append(face_recognition.load_image_file(img_path), name.split(".")[0])
         else:
             print(f"ファイルが見つかりません: {img_path}")
     return train_imgs
 
 def encode_faces(train_imgs):
     known_face_encodings = []
-    known_face_labels = ["Known Person"]
+    known_face_labels = []
 
-    for img in train_imgs:
+    for img , label in train_imgs:
         locations = face_recognition.face_locations(img, model="hog")
         encodings = face_recognition.face_encodings(img, locations)
         known_face_encodings.extend(encodings)
+        known_face_labels.extend([label] * len(encodings))
 
     return known_face_encodings, known_face_labels
 
-def recognize_faces(video_capture, known_face_encodings, known_face_labels, queue):
+def recognize_faces(video_capture, known_face_encodings, known_face_labels, queue, train_img_directory):
+    proess_this_frame = True
+    font_path = "/usr/share/fonts/truetype/note/NotoSansCJK-Regular.ttc"
+
     while True:
         # フレームを取得
         ret, frame = video_capture.read()
@@ -49,19 +56,33 @@ def recognize_faces(video_capture, known_face_encodings, known_face_labels, queu
                     name = known_face_labels[best_match_index]
 
             if name == "Unknown":
-                top, right, bottom, left = [v * 4 for v in face_location]
+                top, right, bottom, left = face_location
+                top *= 4
+                right *= 4
+                bottom *= 4
+                left *= 4
                 face_image = frame[top:bottom, left:right]
-                queue.put("Unknown")  # ここで文字列を送信
-            else:
-                queue.put(name)  # 名前が特定できた場合も文字列を送信
+
+                #サーバーに画像を送信
+                name_from_server = send_to_server(face_image, train_img_directory)
+                if name_from_server:
+                    name = os.path.splitext(name_from_server)[0]
+                else:
+                    name = "Unknown"
+
+            # キューに名前を送信
+            queue.put(name)
 
             # 検出された顔に枠を描画する
             top, right, bottom, left = [v * 4 for v in face_location]  # 元のサイズに戻す
             cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
-            # ラベルを描画する
-            cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-            font = cv2.FONT_HERSHEY_DUPLEX
-            cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+            # フレームに名前を描画するためにPILを使用
+            plil_image = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+            draw = ImageDraw.Draw(plil_image)
+            font = ImageFont.truetype(font_path, 30)
+            draw.text((left + 6, bottom - 35), name, font=font, fill=(255, 255, 255))
+            frame = cv2.cvtColor(np.array(plil_image), cv2.COLOR_RGB2BGR)
 
         # フレームを表示する
         cv2.imshow('Video', frame)
